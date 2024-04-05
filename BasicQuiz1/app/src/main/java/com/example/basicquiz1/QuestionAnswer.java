c752a6c0325727a70917b10ea8e888dedba759a1
AbieAnders <abieanders04@gmail.com>
2024-04-05 15:32:14 +0530

    first commit

---
+++ BasicQuiz1/app/src/main/java/com/example/basicquiz1/QuestionAnswer.java
@@ -0,0 +1,24 @@
+package com.example.basicquiz1;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "Who won the battle of waterloo?",
+            "Which one is not a programming language?",
+            "Which is the largest video streaming platform in the world?",
+            "Which company has the largest market capitalization?"
+    };
+
+    public static String choices[][] = {
+            {"France","Britain","Prussia","Allied Army"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Netflix","TikTok","Instagram","Youtube"},
+            {"Nvidia","Apple","Netflix","Microsoft"}
+    };
+
+    public static String correctAnswers[] = {
+            "Allied Army",
+            "Notepad",
+            "Youtube",
+            "Apple"
+    };
+}
